18609acd5e5f1f882b5240636a6b715412cfd74e
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-05-16 17:31:29 +0200

    just following steps

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,3 @@
-import type { RequestHandler } from "express";
-
 // Load environment variables from .env file
 import "dotenv/config";
 
@@ -10,6 +8,20 @@ import "../database/checkConnection";
 
 // Import the Express application from ./app
 import app from "./app";
+
+/* ************************************************************************* */
+
+// Declaration of a "Welcome" route
+
+import type { RequestHandler } from "express";
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
+
+/* ************************************************************************* */
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
@@ -22,9 +34,3 @@ app
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
   });
-
-const sayWelcome: RequestHandler = (request, response) => {
-  response.send("Welcome to Wild Series !");
-};
-
-app.get("/", sayWelcome);
