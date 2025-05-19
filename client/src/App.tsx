1ee35a0aaae4642d92482a315cb67185b2fe8e0c
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-05-19 16:04:01 +0200

    removing monorepo references

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,24 +5,12 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">JS Monorepo</h1>
+        <h1>Wild series</h1>
       </header>
 
       <main className="text-box">
         <Outlet />
       </main>
-
-      <footer>
-        Développé par la&nbsp;
-        <a
-          href="https://www.wildcodeschool.com/"
-          className="wcs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wild Code School
-        </a>
-      </footer>
     </>
   );
 }
